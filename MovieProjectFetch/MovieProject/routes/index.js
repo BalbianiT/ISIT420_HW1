3b92a2a604792a3a3e6129a16ff4bc8ffdfbc30f
joseph-jennings1 <40963397+joseph-jennings1@users.noreply.github.com>
2022-04-11 21:00:31 -0700

    Add files via upload

--- MovieProjectFetch/MovieProject/routes/index.js
+++ MovieProjectFetch/MovieProject/routes/index.js
@@ -1,18 +1,11 @@
-let x = 2;
+
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 
 // start by creating data so we don't have to type it in each time
-let ServerMovieArray = [];
+let ServerOrderArray = [];
 
-// define a constructor to create movie objects
-let MovieObject = function (pTitle, pYear, pGenre, pMan, pWoman, pURL) {
-    this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
-    this.Title = pTitle;
-    this.Year = pYear;
-    this.Genre = pGenre;  // action  comedy  drama  horrow scifi  musical  western
-}
 
 // my file management code, embedded in an object
 fileManager  = {
@@ -23,24 +16,19 @@ fileManager  = {
   // functions really should take in the name of a file to be more generally useful
   read: function() {
     // has extra code to add 4 movies if and only if the file is empty
-    const stat = fs.statSync('moviesData.json');
+    const stat = fs.statSync('OrdersFiles.json');
     if (stat.size !== 0) {                           
-    var rawdata = fs.readFileSync('moviesData.json'); // read disk file
-    ServerMovieArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
+    var rawdata = fs.readFileSync('OrdersFiles.json'); // read disk file
+    ServerOrderArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
     }
     else {
-      // make up 3 for testing
-      ServerMovieArray.push(new MovieObject("Moonstruck", 1981, "Drama"));
-      ServerMovieArray.push(new MovieObject("Wild At Heart", 1982, "Drama"));
-      ServerMovieArray.push(new MovieObject("Raising Arizona", 1983, "Comedy"));
-      ServerMovieArray.push(new MovieObject("USS Indianapolis", 2016, "Drama"));
-      fileManager.write();
+      console.log("empty file");
     }
   },
   
   write: function() {
-    let data = JSON.stringify(ServerMovieArray);    // take our object data and make it writeable
-    fs.writeFileSync('moviesData.json', data);  // write it
+    let data = JSON.stringify(ServerOrderArray);    // take our object data and make it writeable
+    fs.writeFileSync('OrdersFile.json', data);  // write it
   },
 }
 
@@ -50,18 +38,24 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
 
-/* GET all Movie data */
-router.get('/getAllMovies', function(req, res) {
-  fileManager.read();
-  res.status(200).json(ServerMovieArray);
+
+/* Show one new Order, but doesn't save the Order */
+router.post('/ShowOneOrder', function(req, res) {
+  const newOrder = req.body;  // get the object from the req object sent from browser
+  console.log(newOrder);
+  // prepare a reply to the browser
+  var response = {
+    status  : 200,
+    success : 'Added Successfully'
+  }
+  res.end(JSON.stringify(response)); // send reply
 });
 
-
-/* Add one new Movie */
-router.post('/AddMovie', function(req, res) {
-  const newMovie = req.body;  // get the object from the req object sent from browser
-  console.log(newMovie);
-  ServerMovieArray.push(newMovie);  // add it to our "DB"  (array)
+/* Save one order to file */
+router.post('/StoreOneOrder', function(req, res) {
+  const newOrder = req.body;  // get the object from the req object sent from browser
+  console.log(newOrder);
+  ServerOrderArray.push(newOrder);
   fileManager.write();
   // prepare a reply to the browser
   var response = {
@@ -71,36 +65,12 @@ router.post('/AddMovie', function(req, res) {
   res.end(JSON.stringify(response)); // send reply
 });
 
-// delete movie
+//Get all CD data
+router.get('/getAllCDs', function(req,res) {
+  fileManager.read();
+  res.status(200).json(ServerOrderArray);
+})
 
-router.delete('/DeleteMovie/:ID', (req, res) => {
-  const ID = req.params.ID;
-  let found = false;
-  console.log(ID);    
-
-  for(var i = 0; i < ServerMovieArray.length; i++) // find the match
-  {
-      if(ServerMovieArray[i].ID === ID){
-        ServerMovieArray.splice(i,1);  // remove object from array
-          found = true;
-          fileManager.write();
-          break;
-      }
-  }
-
-  if (!found) {
-    console.log("not found");
-    return res.status(500).json({
-      status: "error"
-    });
-  } else {
-    var response = {
-      status  : 200,
-      success : 'Movie ' + ID + ' deleted!'
-    }
-    res.end(JSON.stringify(response)); // send reply
-  }
-});
 
 
 module.exports = router;
